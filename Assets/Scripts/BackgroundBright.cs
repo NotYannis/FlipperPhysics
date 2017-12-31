95f10e074f4bd81a87e6af1cb5d6ae99413b31df
ATTARD Yannis <yannis.attard@gmail.com>
2017-12-31 16:26:54 +0100

    Add a blonk to the layer background when ball reach the line

--- Assets/Scripts/BackgroundBright.cs
+++ Assets/Scripts/BackgroundBright.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class BackgroundBright : MonoBehaviour {
     private SpriteRenderer spr;
+    public bool flash;
 
-    public float maxAlpha;
+    public float alphaOffset;
+    private float maxAlpha;
     public float alphaChangeTime = 2.0f;
     public float targetAlpha;
     public float smoothAlpha = 0.1f;
 
-    Coroutine moreAlpha;
-    Coroutine lessAlpha;
+    Coroutine moreAlphaCoroutine;
+    Coroutine lessAlphaCoroutine;
 	// Use this for initialization
 	void Awake () {
         spr = GetComponent<SpriteRenderer>();
@@ -25,44 +27,51 @@ public class BackgroundBright : MonoBehaviour {
 
     public void UpdateAlpha(int combo, int maxCombo)
     {
-        maxAlpha = (float)combo / (float)maxCombo;
-        smoothAlpha = (float)combo / (float)maxCombo * 2.0f + 0.1f;
+        maxAlpha = alphaOffset * (float)combo;
         alphaChangeTime = 2.0f - 0.2f * combo;
 
         if(combo == 0)
         {
-            if(lessAlpha != null)
-            {
-                StopCoroutine(lessAlpha);
-            }
-            if(moreAlpha != null)
-            {
-                StopCoroutine(moreAlpha);
-            }
             StartCoroutine(ZeroAlpha());
         }
-        else if(combo == 1)
+        else
         {
-            StartCoroutine(StartChange());
+            if (lessAlphaCoroutine != null)
+            {
+                StopCoroutine(lessAlphaCoroutine);
+            }
+            if (moreAlphaCoroutine != null)
+            {
+                StopCoroutine(moreAlphaCoroutine);
+            }
+
+            if (flash)
+            {
+                smoothAlpha = 1.0f;
+                StartCoroutine(Flash());
+            }
+            else
+            {
+                StartCoroutine(StartChange());
+            }
         }
+
     }
-
-
 
 	IEnumerator StartChange()
     {
-        float time = Random.Range(0.0f, 2.0f);
+        float time = Random.Range(0.0f, alphaChangeTime);
 
         yield return new WaitForSeconds(time);
 
-        moreAlpha = StartCoroutine(MoreAlpha());
+        moreAlphaCoroutine = StartCoroutine(MoreAlpha());
 
         yield return null;
     }
 
+    //Add smoothly alpha to reach maxAlpha, and then call LessAlpha
     IEnumerator MoreAlpha()
     {
-        Color spriteColor = spr.color;
         float cooldown = 0.0f;
 
         while (cooldown < alphaChangeTime)
@@ -70,26 +79,26 @@ public class BackgroundBright : MonoBehaviour {
             cooldown += Time.deltaTime;
             float percent = cooldown / alphaChangeTime;
             targetAlpha = maxAlpha * percent;
-            //spr.color = spriteColor;
             yield return new WaitForEndOfFrame();
         }
 
         yield return new WaitForSeconds(Random.Range(alphaChangeTime / 4.0f, alphaChangeTime / 2.0f));
 
-        lessAlpha = StartCoroutine(LessAlpha());
+        lessAlphaCoroutine = StartCoroutine(LessAlpha());
 
         yield return null;
     }
 
+    //Remove smoothly alpha to reach 0, and then call MoreAlpha
     IEnumerator LessAlpha()
     {
-        Color spriteColor = spr.color;
         float cooldown = alphaChangeTime;
+        float percent;
 
         while (cooldown > 0.0f)
         {
             cooldown -= Time.deltaTime;
-            float percent = cooldown / alphaChangeTime;
+            percent = cooldown / alphaChangeTime;
             targetAlpha = maxAlpha * percent;
             //spr.color = spriteColor;
 
@@ -98,24 +107,61 @@ public class BackgroundBright : MonoBehaviour {
 
         yield return new WaitForSeconds(Random.Range(alphaChangeTime / 4.0f, alphaChangeTime / 2.0f));
         
-        moreAlpha = StartCoroutine(MoreAlpha());
+        moreAlphaCoroutine = StartCoroutine(MoreAlpha());
 
         yield return null;
     }
 
+    //Smoothly set alpha to zero
     public IEnumerator ZeroAlpha()
     {
         Color spriteColor = spr.color;
         float cooldown = 0.5f;
+        float percent;
+        smoothAlpha = 0.1f;
 
         while (cooldown > 0.0f)
         {
             cooldown -= Time.deltaTime;
-            float percent = cooldown / alphaChangeTime;
+            percent = cooldown / alphaChangeTime;
             targetAlpha = maxAlpha * percent;
 
             yield return null;
         }
+
+        yield return null;
+    }
+
+    //Make a little flash using alpha
+    IEnumerator Flash()
+    {
+        float cooldown = 0.0f;
+        float addAlpha = maxAlpha;
+        float flashTime = 0.1f;
+        float percent;
+        float targetAlphaOffset;
+         
+        while (cooldown < 0.1f)
+        {
+            cooldown += Time.deltaTime;
+            percent = cooldown / flashTime;
+            targetAlphaOffset = addAlpha * percent;
+            targetAlpha += targetAlphaOffset;
+            yield return null;
+        }
+
+        while(cooldown >= 0.0f)
+        {
+            percent = cooldown / flashTime;
+            targetAlphaOffset = addAlpha * percent;
+            targetAlpha -= targetAlphaOffset;
+            cooldown -= Time.deltaTime;
+
+            yield return null;
+        }
+
+        smoothAlpha = maxAlpha;
+        StartCoroutine(StartChange());
 
         yield return null;
     }
