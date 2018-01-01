9985a4715e502116555d1bdaeec0e8fd40481836
ATTARD Yannis <yannis.attard@gmail.com>
2018-01-01 13:51:00 +0100

    Added sound on score

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    public static AudioManager Instance;
+
+
+	// Use this for initialization
+	void Start () {
+		if(Instance != null)
+        {
+            print("Error : An instance of AudioManager already exists !");
+            return;
+        }
+        Instance = this;
+	}
+	
+
+
+    private void PlaySound(AudioClip clip)
+    {
+        AudioSource.PlayClipAtPoint(clip, this.transform.position);
+    }
+}
--- Assets/Scripts/Marble/ScoreTrigger.cs
+++ Assets/Scripts/Marble/ScoreTrigger.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class ScoreTrigger : MonoBehaviour {
     ScoreUI scoreUI;
     ObjectPooled scoreParticles;
+    AudioSource scoreSound;
+    float pitch = 1.0f;
+    public float maxPitch = 3.0f;
+    public float pitchOffset = 0.05f;
+    public float pitchResetCooldown = 0.2f;
+
+    Coroutine changePitchCoroutine;
 
 	// Use this for initialization
 	void Start () {
         scoreUI = GameObject.Find("ScoreUI").GetComponent<ScoreUI>();
         scoreParticles = GameObject.Find("ScoreParticlesPool").GetComponent<ObjectPooled>();
+        scoreSound = GetComponent<AudioSource>();
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -22,6 +30,33 @@ public class ScoreTrigger : MonoBehaviour {
 
         scoreParticleInstance.GetComponent<ScoreParticleScript>().PlayParticles(force);
 
+        PlayScoreSound();
+
         collision.gameObject.SetActive(false);
+    }
+
+    void PlayScoreSound()
+    {
+        scoreSound.Play();
+        pitch *= 1.05946f;
+        scoreSound.pitch = pitch;
+        if(changePitchCoroutine != null)
+        {
+            StopCoroutine(changePitchCoroutine);
+        }
+        changePitchCoroutine = StartCoroutine(ChangePitch());
+    }
+
+    IEnumerator ChangePitch()
+    {
+        float cooldown = pitchResetCooldown;
+        while(cooldown > 0.0f)
+        {
+            cooldown -= Time.deltaTime;
+            yield return null;
+        }
+        pitch = 1.0f;
+
+        yield return null;
     }
 }
